4ee33e9bb8223ca256a77332fa2a07f8e8f28365
杨安府 <565870260@qq.com>
2022-01-06 22:18:59 +0800

    :poop: 代码优化

--- packages/taro-vant/src/picker/index.tsx
+++ packages/taro-vant/src/picker/index.tsx
@@ -1,7 +1,5 @@
 import Picker from './Picker'
-import PickerColumn from './PickerColumn'
 
-const PickerNamespace = Object.assign(Picker, { Column: PickerColumn })
-export default PickerNamespace
-export { PickerNamespace as Picker }
+export default Picker
+export { Picker }
 export type { PickerProps, PickerEvents, PickerChangeEvents, PickerColumnProps } from './PropsType'
