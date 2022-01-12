d83a34966dc7da7f41fe004f9966b1997e756852
杨安府 <565870260@qq.com>
2022-01-12 21:48:30 +0800

    fix: sku示例调整

--- packages/taro-vant-demo/src/pages/sku/index.js
+++ packages/taro-vant-demo/src/pages/sku/index.js
@@ -1,51 +1,42 @@
 import { Component } from 'react'
 import { showToast } from '@tarojs/taro'
 import { goods, properties, sku } from './constant'
-import { Sku } from 'taro-vant'
+import { Sku, Cell } from 'taro-vant'
 import DemoPage from '../../components/demo-page/index'
 import DemoBlock from '../../components/demo-block/index'
 import './index.scss'
 
 export default class Index extends Component {
   state = {
-    currentValue: 50,
-  }
-
-  constructor() {
-    super()
-  }
-
-  onChange = (event) => {
-    showToast({
-      icon: 'none',
-      title: `当前值：${event.detail}`,
-    })
-  }
-
-  onDrag = (event) => {
-    this.setState({
-      currentValue: event.detail.value,
-    })
+    visible: false,
   }
 
   render() {
-    const { currentValue } = this.state
+    const { visible } = this.state
+
     return (
       <DemoPage title='Slider 滑块'>
-        <>
-          <DemoBlock title='基础用法'>
-            <Sku
-              visible={true}
-              sku={sku}
-              limit={5}
-              properties={properties}
-              quota={10}
-              stockThreshold={100}
-              onSubmit={(arg1, arg2) => console.log(arg1, arg2)}
-              goods={goods}
-            />
-          </DemoBlock>
-        </>
+        <DemoBlock title='基础用法'>
+          <Cell
+            title='使用'
+            isLink
+            onClick={() =>
+              this.setState({
+                visible: true,
+              })
+            }
+          />
+          <Sku
+            visible={visible}
+            sku={sku}
+            limit={5}
+            properties={properties}
+            quota={10}
+            stockThreshold={100}
+            onSubmit={(arg1, arg2) => console.log(arg1, arg2)}
+            goods={goods}
+          />
+        </DemoBlock>
       </DemoPage>
     )
   }
